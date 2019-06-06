abab1655c4b5b9d85d2d926086da640b72a535f2
luiz <luizhenriquefbb@gmail.com>
2019-06-06 16:10:12 -0300

    post

--- client/cdc-admin/src/components/Main.jsx
+++ client/cdc-admin/src/components/Main.jsx
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import Constantes  from "../Constantes";
 import $ from 'jquery';
+import ComunicaComServer from "../utils/ComunicaComServer";
 
 export default class Main extends Component {
 
     constructor() {
         super();
         this.state = {
+            formNewUser: {
+                nome  : "",
+                email : "",
+                senha : ""
+            },
+
             lista: [
                 { nome: 'Luiz', email: "ahdoahd@dlahsd.com", senha: "1234" },
                 { nome: 'Luiz2', email: "2@dlahsd.com", senha: "2222" },
             ]
         };
+
+
+        // definir o "this" nas funcoes
+        this._getAutores    = this._getAutores.bind(this);
+        this.setNome        = this.setNome.bind(this);
+        this.setEmail       = this.setEmail.bind(this);
+        this.setSenha       = this.setSenha.bind(this);
+        this.atualizaLista  = this.atualizaLista.bind(this);
+        this.enviaForm      = this.enviaForm.bind(this);
+        
+        
     }
     
     /**
      * Chamado quando o componente foi renderizado pela primira vez
      */
-    componentWillMount(){
+    componentDidMount(){
         this._getAutores()
     }
 
@@ -36,6 +54,93 @@ export default class Main extends Component {
         })
     }
 
+    enviaForm(event){
+        // evitar recarregar a pagina
+        event.preventDefault();
+
+        // check form
+        for (const form of document.querySelectorAll("form")) {
+            if(!form.reportValidity()){
+                return;
+            }
+        }
+
+        console.log("Dados sendo enviados");
+
+        // $.ajax({
+        //     url: `${Constantes.BASE_URL}/novo_autor`,
+        //     contentType: "json",
+        //     dataType: "json",
+        //     type: "POST",
+        //     data: {
+        //         nome: "Novo",
+        //         email: "Novo",
+        //         senha: "Novo"
+        //     },
+        //     success: (resposta) => {
+        //         console.log("Enviado com sucesso");
+
+        //     },
+        //     error: (resposta) => {
+        //         console.error("Erro em enviaForm", resposta);
+        //     }
+        // });
+
+        // estava tendo problemas com a API local, por causa do HEADER provavelmente,
+        // e como nao sabia configurar certinho, aproveitei essa classe de outro curso da 
+        // alura
+        ComunicaComServer.post(
+            "novo_autor",
+            {
+                nome  : this.state.formNewUser.nome,
+                email : this.state.formNewUser.email,
+                senha : this.state.formNewUser.senha
+            }
+        );
+    }
+
+    atualizaLista(event){
+        event.preventDefault();
+
+        this._getAutores();
+    }
+
+    setNome(evento){
+        // get old object
+        let formNewUser = this.state.formNewUser;
+
+        // update object
+        formNewUser.nome = evento.target.value;
+
+        // update state
+        this.setState({formNewUser : formNewUser});
+
+    }
+
+    setEmail(evento){
+        // get old object
+        let formNewUser = this.state.formNewUser;
+
+        // update object
+        formNewUser.email = evento.target.value;
+
+        // update state
+        this.setState({formNewUser : formNewUser});
+
+    }
+
+    setSenha(evento){
+        // get old object
+        let formNewUser = this.state.formNewUser;
+
+        // update object
+        formNewUser.senha = evento.target.value;
+
+        // update state
+        this.setState({formNewUser : formNewUser});
+
+    }
+
     render() {
         return (
             <div id="main">
@@ -47,19 +152,20 @@ export default class Main extends Component {
                         <form className="pure-form pure-form-aligned">
                             <div className="pure-control-group">
                                 <label htmlFor="nome">Nome</label>
-                                <input id="nome" type="text" name="nome" value="" />
+                                <input /* required */ id="nome" type="text" name="nome" value={this.state.formNewUser.nome} onChange={this.setNome}  />
                             </div>
                             <div className="pure-control-group">
                                 <label htmlFor="email">Email</label>
-                                <input id="email" type="email" name="email" value="" />
+                                <input /* required */ id="email" type="email" name="email" value={this.state.formNewUser.email} onChange={this.setEmail} />
                             </div>
                             <div className="pure-control-group">
                                 <label htmlFor="senha">Senha</label>
-                                <input id="senha" type="password" name="senha" />
+                                <input /* required */ id="senha" type="password" name="senha" value={this.state.formNewUser.senha} onChange={this.setSenha}/>
                             </div>
                             <div className="pure-control-group">
                                 <label></label>
-                                <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                                <button onClick={this.enviaForm} className="pure-button pure-button-primary">Gravar</button>
+                                <button onClick={this.atualizaLista} className="pure-button pure-button-primary">Atualizar</button>
                             </div>
                         </form>
 
@@ -74,8 +180,8 @@ export default class Main extends Component {
                             </thead>
                             <tbody>
                                 {
-                                    this.state.lista.map(el => {
-                                        return  <tr>
+                                    this.state.lista.map((el, index) => {
+                                        return  <tr key={index}>
                                                     <td>{el.nome}</td>
                                                     <td>{el.email}</td>
                                                 </tr>
---
+++ client/cdc-admin/src/utils/ComunicaComServer.jsx
@@ -0,0 +1,97 @@
+import Constantes from "../Constantes";
+
+export default class ComunicaComServer {
+    constructor() {
+        throw new Error("Não instanciar");
+    }
+
+    /**
+     * Get ajax com vanillaJS
+     * @param {*} url 
+     * @param {Function} callback O que fazer com o dado recebido
+     * @param {Array} params Lista de parametros para o callback
+     * @returns {JSON}
+     */
+    static get(url) {
+        // url = `${ComunicaComServer.burl}/${url}`;
+
+        // retornar a promisse
+        return new Promise((resolve, reject) => {
+
+            let dataFromReturn = {};
+
+            let xhr = new XMLHttpRequest();
+
+            xhr.open("GET", url);
+
+
+            // configurar comunicacao
+            xhr.onreadystatechange = () => {
+                // 0: requisição ainda não iniciada.
+                // 1: conexão com o servidor estabelecida.
+                // 2: requisição recebida.
+                // 3: processando requisição.
+                // 4: requisição concluída e a resposta esta pronta.
+                if (xhr.readyState === 4) {
+
+                    console.log('xhr.status', xhr.status);
+
+                    // confirma que a requisicao ta OK. Teve resposta
+                    if (xhr.status === 200) {
+                        dataFromReturn = JSON.parse(xhr.responseText);
+                        console.log('Resposta: ', JSON.parse(xhr.responseText));
+
+                        resolve(dataFromReturn);
+
+                    }
+                    else {
+                        const msg = `Não obteve resposta do servidor. Url: ${url}`
+                        console.log('xhr: ', xhr);
+
+                        reject({ "teveErro": true, "msg": msg });
+                    }
+
+                    return dataFromReturn;
+                }
+
+
+
+            };
+
+
+            xhr.send();
+
+            // possivel colocar um timeout na requisicao
+            // setTimeout(() => reject('HOUVE PROBLEMAS'), 5000);
+
+        });
+    }
+
+    static post(url, data) {
+        url = `${Constantes.BASE_URL}/novo_autor`
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+    
+            xhr.onreadystatechange = () => {
+    
+                if (xhr.readyState === 4) {
+    
+                    if (xhr.status === 200) {
+                        resolve({ msg: 'Negociação enviada com sucesso' });
+    
+                    } else {
+                        reject({ "msg": `Não foi possível enviar a negociação: ${xhr.responseText}` });
+                    }
+                }
+            }
+
+            xhr.send(JSON.stringify(data));
+
+            // possivel colocar um timeout na requisicao
+            // setTimeout(() => reject('HOUVE PROBLEMAS'), 5000);
+
+        });
+    }
+}
